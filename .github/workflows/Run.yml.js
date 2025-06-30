2506ef4f3fda693b9ea8ff8900966c80d3bcb171
IMRAN AHMED <137718252+MR-IMRAN-60@users.noreply.github.com>
2025-06-30 19:13:49 +0600

    Create Run.yml.js

---
+++ .github/workflows/Run.yml.js
@@ -0,0 +1,26 @@
+name: IMRAN-BOT
+
+on:
+  push:
+    branches: [ main ]
+  pull_request:
+    branches: [ main ]
+
+jobs:
+  build:
+    runs-on: ubuntu-latest
+
+    steps:
+      - name: Checkout code
+        uses: actions/checkout@v3
+
+      - name: Set up Node.js
+        uses: actions/setup-node@v3
+        with:
+          node-version: '16'
+
+      - name: Install dependencies
+        run: npm install
+
+      - name: IMRAN-BOT
+        run: npm start
