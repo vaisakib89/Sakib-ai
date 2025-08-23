5b703a5b6eec438ffabc42d8075938789ed1ae18
vaisakib89 <vaisakib499@gmail.com>
2025-08-24 04:04:41 +0600

    Update SAKIBC.js

--- main/catalogs/SAKIBC.js
+++ main/catalogs/SAKIBC.js
@@ -1,55 +1,54 @@
 const chalk = require('chalk');
-const gradient= require('gradient-string');
-const chalkAnimation = require('chalkercli');
-const color = gradient('blue', 'purple');
-const crayon = gradient('yellow', 'lime', 'green');
-const pprl = gradient('#00FFFF', '#00FFFF');
-const blu = gradient("#243aff", "#4687f0", "#5800d4");
-const sky = gradient('#0905ed','#346eeb', '#344feb');
+const gradient = require('gradient-string');
 const config = require("../configs/console.json");
-var successColor = config.console.success;
-const errorColor = config.console.error;
-const warnColor = config.console.warn;
 
+// Colors from config
+const successColor = config.console.success || 'green';
+const errorColor = config.console.error || 'red';
+const warnColor = config.console.warn || 'yellow';
+
+// Main logger function
 module.exports = (text, type) => {
-  switch (type) {
-		case "warn":
-			process.stderr.write(chalk[`${warnColor}`](config.console.editNames.warn) + ` - ${text}\n`);
-			break;
-		case "error":
-			process.stderr.write(chalk[`${errorColor}`](config.console.editNames.error) + ` - ${text}\n`);
-			break;
-		case "load":
-      process.stderr.write(chalk[`${successColor}`]('new user') + `- ${text}\n`);
-			break;
-		default:
-			process.stderr.write(chalk[`${successColor}`](type) + ` - ${text}\n`);
-			break;
-	}
-};
-module.exports.error = (text, type) => {
-	process.stderr.write(chalk[`${errorColor}`](config.console.editNames.error) + ` - ${text}\n`);
+    switch (type) {
+        case "warn":
+            process.stderr.write(chalk[warnColor](config.console.editNames.warn) + ` - ${text}\n`);
+            break;
+        case "error":
+            process.stderr.write(chalk[errorColor](config.console.editNames.error) + ` - ${text}\n`);
+            break;
+        case "load":
+            process.stderr.write(chalk[successColor]('new user') + ` - ${text}\n`);
+            break;
+        default:
+            process.stderr.write(chalk[successColor](type) + ` - ${text}\n`);
+            break;
+    }
 };
 
-module.exports.err = (text, type) => {
-  process.stderr.write(chalk[`${errorColor}`](config.console.editNames.error) + ` - ${text}\n`);
+// Shortcut methods
+module.exports.error = (text) => {
+    process.stderr.write(chalk[errorColor](config.console.editNames.error) + ` - ${text}\n`);
 };
 
-module.exports.warn = (text, type) => {
-	process.stderr.write(chalk[`${warnColor}`](config.console.editNames.warn) + ` - ${text}\n`);
+module.exports.err = (text) => {
+    process.stderr.write(chalk[errorColor](config.console.editNames.error) + ` - ${text}\n`);
 };
 
+module.exports.warn = (text) => {
+    process.stderr.write(chalk[warnColor](config.console.editNames.warn) + ` - ${text}\n`);
+};
 
+// Loader function
 module.exports.loader = (data, option) => {
-	switch (option) {
-		case "warn":
-			process.stderr.write(chalk[`${warnColor}`](config.console.editNames.warn) + ` - ${data}\n`);
-			break;
-		case "error":
-			process.stderr.write(chalk[`${errorColor}`](`${config.console.editNames.error}`) + ` - ${data}\n`);
-			break;
-		default:
-			process.stderr.write(chalk[`${successColor}`](`${config.console.editNames.success}`) + ` - ${data}\n`);
-			break;
-	}
-}
+    switch(option) {
+        case "warn":
+            process.stderr.write(chalk[warnColor](config.console.editNames.warn) + ` - ${data}\n`);
+            break;
+        case "error":
+            process.stderr.write(chalk[errorColor](config.console.editNames.error) + ` - ${data}\n`);
+            break;
+        default:
+            process.stderr.write(chalk[successColor](config.console.editNames.success) + ` - ${data}\n`);
+            break;
+    }
+};
