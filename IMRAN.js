e81c2253459c86c9e83d994b550c4097873d9286
vaisakib89 <vaisakib499@gmail.com>
2025-08-24 01:33:32 +0600

    Update IMRAN.js

--- IMRAN.js
+++ IMRAN.js
@@ -1,5 +1,5 @@
 module.exports = async ({ api, event }) => {
-  const logger = require('./main/catalogs/IMRANC.js')
+  const logger = require('./main/catalogs/SAKIBC.js')
   
   const configCustom = {
     autosetbio: {
