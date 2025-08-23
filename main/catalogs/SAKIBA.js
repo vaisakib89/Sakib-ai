67cfb9eaa63042a6303c55343d1a9f482a63019b
vaisakib89 <vaisakib499@gmail.com>
2025-08-24 01:16:29 +0600

    Update and rename IMRANA.js to SAKIBA.js

--- main/catalogs/SAKIBA.js
+++ main/catalogs/SAKIBA.js
@@ -3,7 +3,7 @@ const { spawn } = require("child_process");
 const express = require("express");
 const app = express();
 const chalk = require('chalk');
-const logger = require("./IMRANC.js");
+const logger = require("./SAKIBC.js");
 const path = require('path'); 
 const PORT = process.env.PORT || 8080 || 9000 || 5555 || 5050 || 5000 || 3003 || 2000 || 1029 || 1010;
 app.get('/', function(req, res) {
@@ -15,7 +15,7 @@ function startBot(message) {
   console.log(chalk.blue('DEPLOYING MAIN SYSTEM'));
   logger.loader(`deploying app on port ${chalk.blueBright(PORT)}`);
   app.listen(logger.loader(`app deployed on port ${chalk.blueBright(PORT)}`));
-  const child = spawn("node", ["--trace-warnings", "--async-stack-traces", "IMRANB.js"], {
+  const child = spawn("node", ["--trace-warnings", "--async-stack-traces", "SAKIBB.js"], {
         cwd: __dirname,
         stdio: "inherit",
         shell: true
